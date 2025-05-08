04acbe89950c9670fde1ea8897c60d28e9a9f54d
BlackFrisian13 <sonaivanyan13@gmail.com>
2025-05-08 14:37:37 +0200

    Search bar and icon implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
 import {Image, ScrollView, Text, View} from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter} from "expo-router";
 
 export default function Index() {
+    const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
         <Image source={images.bg} className="absolute w-full z-0"/>
@@ -11,6 +15,13 @@ export default function Index() {
             paddingBottom: 10
         }} >
             <Image source={icons.logo} className="w-12 h-10 mt-20 mg-5 mx-auto" />
+
+            <View className="flex-1 mt-5">
+                <SearchBar
+                    onPress={() => router.push('/search')}
+                    placeholder={"Search for a movie"}
+                />
+            </View>
         </ScrollView>
     </View>
   );
