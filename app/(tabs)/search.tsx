f39930b481e87b96686063fd8ca88e4917c205de
BlackFrisian13 <sonaivanyan13@gmail.com>
2025-05-10 19:54:19 +0200

    Debounce search term

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -19,15 +19,15 @@ const Search = () => {
     } = useFetch(() => fetchMovies({query: searchQuery}), false);
 
     useEffect(() => {
-        const func = async () =>{
+        const timeoutId = setTimeout (async () =>{
             if(searchQuery.trim()){
                 await loadMovies();
             } else {
                 reset();
             }
-        }
+        }, 500);
 
-        func();
+        return () => clearTimeout(timeoutId);
     }, [searchQuery]);
 
     return (
