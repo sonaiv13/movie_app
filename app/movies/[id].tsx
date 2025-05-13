5dab4b0cdd4031f0aa2e3fc560ea1c8252d3fa83
BlackFrisian13 <sonaivanyan13@gmail.com>
2025-05-13 22:24:41 +0200

    Button 'play' set up

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,4 +1,4 @@
-import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import {Button, Image, ScrollView, Text, TouchableHighlight, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {router, useLocalSearchParams} from "expo-router";
 import {fetchMovieDetails} from "@/services/api";
@@ -27,18 +27,26 @@ const MovieDetails = () => {
             <ScrollView contentContainerStyle={{
                 paddingBottom: 80
             }}>
-                <View>
+                <View className="relative">
                     <Image
                         source={{ uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`}}
                         className="w-full h-[550px]"
                         resizeMode="stretch"
                     />
+
+                    <TouchableHighlight className="absolute bottom-[-28px] right-5 bg-white rounded-full w-14 h-14 items-center justify-center z-10"
+                                        underlayColor="#e5e5e5"
+                                        onPress={() => console.log('Play')}>
+                        <Image source={icons.play} className="size-6" />
+                    </TouchableHighlight>
                 </View>
+
 
                 <View className="flex-col items-start justify-center mt-5 mb-10 px-5">
                     <Text className="text-white font-bold text-xl">{movie?.title}</Text>
                     <View className="flex-row items-center gap-x-1 mt-2">
                         <Text className="text-light-200 text-sm">{movie?.release_date?.split('-')[0]}</Text>
+                        <Text className="text-light-200 text-sm"> · </Text>
                         <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
                     </View>
                     <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
